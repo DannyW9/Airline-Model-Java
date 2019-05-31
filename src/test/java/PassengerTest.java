bb42952a257f84f344191a004cec06593ef80843
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-31 11:09:54 +0100

    Adds basic setup for Passenger class with basic get tests passing

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    private Passenger passenger;
+
+    @Before
+    public void setUp() {
+        passenger = new Passenger("Dave", 2);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Dave", passenger.getName());
+    }
+
+    @Test
+    public void hasBags() {
+        assertEquals(2, passenger.getBags());
+    }
 }
