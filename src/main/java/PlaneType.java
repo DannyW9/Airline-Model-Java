d5ee38798443c6b64cf9470024f3c539d85efbfa
Danny Welsh <dannywelsh400@hotmail.com>
2019-05-31 11:25:00 +0100

    Adds PlaneType enum, as well as setting up the Plane class with basic tests passing

--- src/main/java/PlaneType.java
+++ src/main/java/PlaneType.java
@@ -1,2 +1,22 @@
 public enum PlaneType {
+
+    BOEING747(5, 20),
+    AIRBUS340(10, 50),
+    BEECHCRAFT1900(3, 10);
+
+    private final int capacity;
+    private final int totalWeight;
+
+    PlaneType(int capacity, int totalWeight){
+        this.capacity = capacity;
+        this.totalWeight = totalWeight;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int getTotalWeight() {
+        return this.totalWeight;
+    }
 }
--- src/test/java/PlaneTest.java
+++ src/test/java/PlaneTest.java
@@ -1,2 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PlaneTest {
+
+    private Plane plane;
+
+    @Before
+    public void setUp() {
+        plane = new Plane(PlaneType.BOEING747);
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(5, plane.getCapacity());
+    }
+
+    @Test
+    public void hasTotalWeight() {
+        assertEquals(20, plane.getTotalWeight());
+    }
 }
